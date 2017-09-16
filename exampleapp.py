bc63070ded237af49501b0668cf35b41fee4dfcf
Joe Heck <heckj@mac.com>
2017-09-16 11:13:34 -0700

    adding debug on and exposing dev server to all interfaces

--- exampleapp.py
+++ exampleapp.py
@@ -11,3 +11,6 @@ def index():
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
