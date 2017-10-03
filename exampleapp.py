6a182aad36911910d619424963f209d636c58086
Joe Heck <heckj@mac.com>
2017-10-03 15:56:45 -0700

    adding code to leverage ConfigMap data

--- exampleapp.py
+++ exampleapp.py
@@ -1,5 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+from configparser import SafeConfigParser
+from pathlib import Path
+
 from flask import Flask
 from flask import render_template
+
+parser = SafeConfigParser(os.environ)
+config_file = Path(os.environ.get('CONFIG_FILE','/opt/feature.flags'))
+
+if config_file.is_file():
+    parser.read(os.environ.get('CONFIG_FILE'))
 
 app = Flask(__name__)
 
@@ -10,7 +22,10 @@ def index():
 @app.route('/hello')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('hello.html',
+            greeting=parser.get('features','greeting',fallback="Howdy"),
+            name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    debug_enable = parser.getboolean('feaxtures','debug',fallback=False)
+    app.run(debug=debug_enable,host='0.0.0.0')
