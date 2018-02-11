a43a226d04b6eaf4f98dff59948b4fa894034dcc
Joe Heck <heckj@mac.com>
2018-02-11 15:53:13 -0800

    adding tracing

--- exampleapp.py
+++ exampleapp.py
@@ -8,6 +8,10 @@ from pathlib import Path
 
 from flask import Flask
 from flask import render_template, make_response, request
+
+from jaeger_client import Config
+from flask_opentracing import FlaskTracer
+
 from prometheus_client import Summary, Counter, Histogram
 from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
@@ -20,6 +24,15 @@ FLASK_REQUEST_LATENCY = Histogram('flask_request_latency_seconds', 'Flask Reques
 				['method', 'endpoint'])
 FLASK_REQUEST_COUNT = Counter('flask_request_count', 'Flask Request Count',
 				['method', 'endpoint', 'http_status'])
+
+def initialize_tracer():
+    config = Config(
+        config={
+            'sampler' :{'type': 'const', 'param': 1}
+        },
+        service_name='flask-service'
+    )
+    return config.initialize_tracer() # also sets opentracing.tracer
 
 def before_request():
     request.start_time = time.time()
@@ -95,4 +108,5 @@ if __name__ == '__main__':
     redis_store = redis.StrictRedis(host=redis_host, port=6379, db=0)
     app.before_request(before_request)
     app.after_request(after_request)
+    flask_tracer = FlaskTracer(initialize_tracer, True, app)
     app.run(debug=debug_enable, host='0.0.0.0')
