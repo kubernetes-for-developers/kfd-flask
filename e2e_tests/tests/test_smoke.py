07ed28a2e09b3d4ba796ca06e08389417a7583a4
Joe Heck <heckj@mac.com>
2018-02-25 16:38:37 -0800

    misc test examples to work things out...

--- e2e_tests/tests/test_smoke.py
+++ e2e_tests/tests/test_smoke.py
@@ -8,7 +8,24 @@ def kube_v1_client():
     v1 = kubernetes.client.CoreV1Api()
     return v1
 
+def test_contexts(kube_v1_client):
+    contexts, active_context = kubernetes.config.list_kube_config_contexts()
+    #print("%s\t%s" % (contexts, active_context))
+    #print(kube_v1_client.list_node())
+    #print("-----------------------------------------------------------------")
+    print(kube_v1_client.list_service_for_all_namespaces())
+    print(kubernetes.config)
+
+def test_component_status(kube_v1_client):
+    ret = kube_v1_client.list_component_status()
+    for item in ret.items:
+        assert item.conditions[0].type == "Healthy"
+        print("%s: %s" % (item.metadata.name, item.conditions[0].type))
+
 def test_noop(kube_v1_client):
     assert 1
-    kube_v1_client.list_pod_for_all_namespaces(watch=False)
+    ret = kube_v1_client.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        print("%s\t%s\t%s" %
+              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
     assert 1
